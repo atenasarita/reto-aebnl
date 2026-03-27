0730d3b4b8002bf87935c001b8780d0b739edbd6
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-03-27 14:20:26 -0600

    creacion de trial page para backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
 import GestionBeneficiarios from './pages/GestionBeneficiarios'
+import TrialBackend from './pages/TrialBackend'
 
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/beneficiarios" element={<GestionBeneficiarios />} />
+          <Route path="/trial" element={<TrialBackend />} />
         </Route>
       </Routes>
     </BrowserRouter>
