a2524073b01451ff292fa43b5c989fcae841f42a
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-04-27 13:49:20 -0600

    fix: modificaciones en port desde index.ts

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ import dashboardRoutes from "./src/routes/dashboard.routes";
 import { startMembresiaExpirationJob } from './src/jobs/membresiaExpiration.job';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
