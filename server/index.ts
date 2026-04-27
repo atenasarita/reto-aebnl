60b463b9a5d3c1b241985ce4e849880ba1ed7e34
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-04-27 17:09:16 -0600

    temporary fix

--- server/index.ts
+++ server/index.ts
@@ -32,7 +32,7 @@ app.use('/api', dashboardRoutes);
 
 app.use(errorMiddleware);
 
-startMembresiaExpirationJob();
+// startMembresiaExpirationJob();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
