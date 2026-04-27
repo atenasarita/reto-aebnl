24f3cb99fe84d2cb7c2f49b0689b2de6c0ea07c6
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-04-27 17:40:30 -0600

    trial railway

--- server/index.ts
+++ server/index.ts
@@ -85,3 +85,9 @@ startMembresiaExpirationJob();
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+*/
+
+app.get("/", (req, res) => {
+  res.send("OK 🚀");
+});
