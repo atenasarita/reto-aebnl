78a75c067597f5995589c69d2359b8ca73de32a5
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-04-28 13:45:26 -0600

    fix: comentario de index para deployment

--- server/index.ts
+++ server/index.ts
@@ -44,8 +44,19 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+
+app.get("/", (req, res) => {
+  res.send("OK 🚀");
+});
 */
 
 app.get("/", (req, res) => {
   res.send("OK 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
