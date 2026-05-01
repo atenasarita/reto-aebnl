7383abb0a667180f70c1a87b8d944e9024600d4e
RosyMsp <irosesp7@gmail.com>
2026-04-30 18:49:53 -0600

    Primer avance Detalle Cita

--- server/src/repositories/citas.queries.ts
+++ server/src/repositories/citas.queries.ts
@@ -1,17 +1,20 @@
 export const citasQueries ={
     getCitas: `
     SELECT 
-        id_cita,
-        id_beneficiario,
-        fecha,
-        hora,
-        id_especialista,
-        id_catalogo_servicio,
-        motivo,
-        notas,
-        estatus
-    FROM citas
-    ORDER BY fecha DESC, hora DESC
+    id_cita AS "id",
+    motivo AS "title",
+    TO_CHAR(fecha, 'YYYY-MM-DD') || 'T' ||
+    CASE 
+        WHEN LENGTH(hora) = 5 THEN hora || ':00'
+        ELSE hora
+    END AS "start",
+    id_beneficiario AS "idBeneficiario",
+    id_especialista AS "id_especialista",
+    id_catalogo_servicio AS "idServicio",
+    notas AS "notas",
+    estatus AS "estatus"
+FROM citas
+ORDER BY fecha DESC, hora DESC;
     `.trim(),
 
     insertCita: `
