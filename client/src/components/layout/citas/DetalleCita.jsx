f563ce99171f9b467da7fa59ef02780f5cb11590
RosyMsp <irosesp7@gmail.com>
2026-05-05 10:56:51 -0600

    Detalle cita front

--- client/src/components/layout/citas/DetalleCita.jsx
+++ client/src/components/layout/citas/DetalleCita.jsx
@@ -1,3 +1,4 @@
+import "./detalleCita.css"
 export default function DetalleCita({cita, onClose}){
     if (!cita) return null;
 
@@ -12,10 +13,14 @@ export default function DetalleCita({cita, onClose}){
                 <p><strong>Motivo:</strong>{cita.title}</p>
                 <p><strong>Fecha y Hora:</strong>{cita.start?.toLocaleString()}</p>
                 <p><strong>Beneficiario:</strong>{cita.extendedProps?.idBeneficiario}</p>
-                <p><strong>Especialista:</strong>{cita.extendedProps?.id_especialista}</p>
+                <p><strong>Especialista:</strong>{cita.extendedProps?.especialista}</p>
                 <p><strong>Servicio:</strong>{cita.extendedProps?.idServicio}</p>
                 <p><strong>Estatus:</strong>{cita.extendedProps?.estatus}</p>
                 <p><strong>Notas:</strong>{cita.extendedProps?.notas || "Sin notas"}</p>
+
+                <button className="modificar-cita-btn">
+                    Modificar cita
+                </button>
             </div>
         </div>
     );
