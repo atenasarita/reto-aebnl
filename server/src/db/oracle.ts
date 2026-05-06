2ac71cdeca36f5167029f2764eb5a3c4933f40ec
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-05-05 19:19:39 -0600

    fix en oracle connection

--- server/src/db/oracle.ts
+++ server/src/db/oracle.ts
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-process.env.TNS_ADMIN = process.env.TNS_ADMIN || "/opt/render/project/src/wallet";
-
 export class OracleConnection {
     async getConnection() {
         const user = process.env.ORACLE_USER;
         const password = process.env.ORACLE_PASSWORD;
         const connectString = process.env.ORACLE_CONNECT_STRING;
+        const walletPassword = process.env.ORACLE_WALLET_PASSWORD;
+        const walletLocation = process.env.TNS_ADMIN || '/tmp/wallet';
 
         if (!user || !password || !connectString) {
             throw new Error('Faltan variables de entorno de Oracle.');
@@ -18,31 +18,9 @@ export class OracleConnection {
         return oracledb.getConnection({
             user,
             password,
-            connectString
+            connectString,
+            walletLocation,
+            walletPassword,
         });
     }
 }
-
-export async function testConnection() {
-    console.log("executing testConnection");
-
-    const connectionService = new OracleConnection();
-    let connection;
-    try {
-        connection = await connectionService.getConnection();
-        console.log("Successfully connected to Oracle Database");
-    } catch (err) {
-        console.error(err);
-    } finally {
-        if (connection) {
-            try {
-                await connection.close();
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    }
-}
-
-
-
