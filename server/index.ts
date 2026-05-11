823b67e8788ee39566748c1e41a679822b1f94d5
RosyMsp <irosesp7@gmail.com>
2026-05-11 16:16:14 -0600

    Cambios en index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import cors from 'cors';
 import 'dotenv/config';
 import usuariosRoutes from "./src/routes/usuarios.routes";
@@ -17,8 +16,6 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
