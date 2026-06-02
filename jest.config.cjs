f21449f44b71090cd13f580c6dcf3ed513f017a3
RosyMsp <irosesp7@gmail.com>
2026-06-02 15:24:20 -0600

    cambio de rutas

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-//   testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.mjs', '**/?(*.)+(spec|test).js', '**/?(*.)+(spec|test).mjs'],
-  testMatch: ['<rootDir>/unitTests/__tests__/**/*.test.mjs'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.mjs', '**/?(*.)+(spec|test).js', '**/?(*.)+(spec|test).mjs'],
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'json', 'node'],
    roots: ['<rootDir>/client/src', '<rootDir>/unitTests/__tests__'],
    moduleDirectories: ['node_modules', 'client/node_modules'],
