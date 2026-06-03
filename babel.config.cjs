f9e6788f0f8f57b6124e329614a34eaf1a62d571
RosyMsp <irosesp7@gmail.com>
2026-06-02 20:20:03 -0600

    cambios config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+        modules: false,
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
   ],
 };
