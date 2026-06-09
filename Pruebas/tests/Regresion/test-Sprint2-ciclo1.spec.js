1adba5c35672e1092d50d6813816ae3b2acedf6b
Karen B <a00840405@tec.mx>
2026-06-09 15:02:57 -0600

    Regresion sprint 2 ciclo 1

--- Pruebas/tests/Regresion/test-Sprint2-ciclo1.spec.js
+++ Pruebas/tests/Regresion/test-Sprint2-ciclo1.spec.js
@@ -4,12 +4,12 @@ import path from 'path';
 import fs from 'fs';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
+  await page.goto('https://aebnl.netlify.app/login');
   await page.getByRole('textbox', { name: 'Usuario' }).click();
   await page.getByRole('textbox', { name: 'Usuario' }).fill('prueba1');
-  await page.getByRole('textbox', { name: '********' }).click();
-  await page.getByRole('textbox', { name: '********' }).fill('admin1');
-  await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('admin1');
+  await page.getByRole('button', { name: 'Entrar al sistema' }).click();
 });
 
 // Recibos
@@ -28,7 +28,7 @@ test('HU - 023 - Mostrar mensaje sin registros', async ({ page }) => {
     qase.id(85);
     await test.step('Given que no existen servicios registrados en la fecha seleccionada', async () => {
         await page.getByRole('button', { name: 'Recibos', exact: true }).click();
-        await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-28');
+        await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-02-28');
     });
     await test.step('When el usuario consulta el historial de recibos', async () => {
         await page.getByRole('heading', { name: 'Recibos del día' }).click();
@@ -48,11 +48,11 @@ test('HU - 023 - Visualización de recibos por fecha seleccionada ', async ({ pa
 
 test('HU - 023 - Actualizar historial al cambiar fecha', async ({ page }) => {
     qase.id(86);
-    await page.getByRole('button', { name: 'Recibos Control de pagos y' }).click();
+    await page.getByRole('button', { name: 'Recibos', exact: true }).click();
     await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-13');
     await expect(page.getByText('Mostrando 1 de 1 recibos · 13 may')).toBeVisible();
     await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-21');
-    await expect(page.getByText('Mostrando 17 de 17 recibos · 21 may')).toBeVisible();
+    await expect(page.getByText('Mostrando 18 de 18 recibos · 21 may')).toBeVisible();
 });
 
 test('HU - 023 - Validar información de cada recibo', async ({ page }) => {
@@ -65,14 +65,15 @@ test('HU - 023 - Validar información de cada recibo', async ({ page }) => {
         await page.getByRole('button', { name: 'Ver detalle del recibo 86' }).click();
     });
     await test.step('Then cada registro debe incluir beneficiario, tipo de servicio, hora y estatus', async () => {
-        await expect(page.getByText('Folio #86Sofio RamirezCONSULTA DENTAL · 15 may 2026 00:26✕')).toBeVisible();
+        await expect(page.getByText('Folio #')).toBeVisible();
+        await expect(page.getByText('CONSULTA DENTAL · 15 may 2026 00:')).toBeVisible();
     });
 });
 
 test('HU - 023 - Evitar registros de fechas incorrectas ', async ({ page }) => {
     qase.id(90);
     await test.step('Given que existen recibos registrados en diferentes fechas', async () => {
-        await page.getByRole('button', { name: 'Recibos Control de pagos y' }).click();
+        await page.getByRole('button', { name: 'Recibos', exact: true }).click();
     });
     await test.step('When el usuario selecciona una fecha específica', async () => {
         await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-15');
@@ -91,20 +92,20 @@ test('HU - 009 - Visualizar reporte mensual de personas atendidas', async ({ pag
         await page.getByRole('link', { name: 'Período' }).click();
     });
     await test.step('When selecciona una opción de fecha mensual', async () => {
-        await page.getByText('Servicios otorgados por díaMayo').click();
+        await page.getByText('Servicios otorgados por díaJunio').click();
     });
     await test.step('Then el sistema debe mostrar en pantalla la gráfica de personas atendidas por mes', async () => {
-        await page.locator('div').filter({ hasText: 'Nuevos beneficiarios61' }).nth(4).click();
-        await page.getByRole('paragraph').filter({ hasText: '61' }).click();
-        await page.locator('div').filter({ hasText: 'Total atendidos11' }).nth(4).click();
-        await page.getByText('11').nth(1).click();
-        await page.getByText('Servicios otorgados por díaMayo').click();
-        await page.getByText('Mayo 2026').click();
+        await page.locator('div').filter({ hasText: 'Nuevos beneficiarios' }).nth(4).click();
+        await page.getByRole('paragraph').filter({ hasText: '8' }).click();
+        await page.locator('div').filter({ hasText: 'Total atendidos' }).nth(4).click();
+        await page.getByText('13').nth(1).click();
+        await page.getByText('Servicios otorgados por díaJunio').click();
+        await page.getByText('Junio 2026').click();
         // await expect(page.locator('section')).toMatchAriaSnapshot(`- application: /1 2 3 4 5 6 7 8 9 \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ 0 4 8 \\d+ \\d+/`);
     await page.getByRole('article').filter({ hasText: 'Reporte Mensual de' }).click();
     await expect(page.locator('section')).toMatchAriaSnapshot(`
         - heading "Servicios otorgados por día" [level=3]
-        - text: /Mayo \\d+/
+        - text: /Junio \\d+/
         `);
     });
 });
@@ -126,7 +127,6 @@ test('HU - 009 - Visualizar reporte anual de personas atendidas', async ({ page 
         await page.getByRole('button', { name: 'Nuevos beneficiarios' }).click();
         await expect(page.getByRole('heading', { name: 'Nuevos beneficiarios por mes' })).toBeVisible();
         await expect(page.getByText('Total atendidos')).toBeVisible();
-        await expect(page.getByText('13').first()).toBeVisible();
     });
 });
 
@@ -140,9 +140,9 @@ test('HU - 009 - Visualizar reporte por rango de fechas', async ({ page }) => {
         await page.getByRole('button', { name: 'Generar reporte' }).click();
     });
     await test.step('Then el sistema debe mostrar la información de personas atendidas correspondiente a ese periodo', async () => {
-        await page.getByRole('application').filter({ hasText: '05-0105-0405-0705-1005-1305-' }).click();
-        await page.getByLabel('Resumen de configuración del').getByText('may 2026 — 31 may 2026').click();
-        await page.getByRole('application').filter({ hasText: '05-0105-0405-0705-1005-1305-' }).click();
+        await page.getByRole('application').filter({ hasText: '06-0106-0406-0706-1006-1306-' }).click();
+        await page.getByText('jun 2026 — 30 jun 2026').click();
+        await page.getByRole('application').filter({ hasText: '06-0106-0406-0706-1006-1306-' }).click();
     });
 });
 
@@ -153,11 +153,11 @@ test('HU - 009 - Exportar reporte en formato CSV', async ({ page }) => {
         await page.getByRole('link', { name: 'Período' }).click();
     });
     await test.step('And existe información disponible en el reporte', async () => {
-        await page.getByText('Servicios otorgados por díaMayo').click();
+        await page.getByText('Servicios otorgados por díaJunio').click();
     });
     await test.step('When presiona el botón de exportar Then el sistema debe generar un archivo en formato .csv', async () => {
         const downloadPromise = page.waitForEvent('download');
-        await page.getByRole('button', { name: 'Exportar' }).click();
+        await page.getByRole('button', { name: 'Exportar CSV' }).click();
         const download = await downloadPromise;
         const downloadPath = path.join('test_results','reporte_general', download.suggestedFilename());
         await download.saveAs(downloadPath);
@@ -174,11 +174,11 @@ test('HU - 009 - Validar contenido del archivo CSV', async ({ page }) => {
     await page.getByRole('button', { name: 'Reportes' }).click();
     await page.getByRole('link', { name: 'Período' }).click();
 
-    await expect(page.getByText('Servicios otorgados por díaMayo')).toBeVisible();
+    await expect(page.getByText('Servicios otorgados por díaJunio')).toBeVisible();
 
     const downloadPromise = page.waitForEvent('download');
 
-    await page.getByRole('button', { name: 'Exportar' }).click();
+    await page.getByRole('button', { name: 'Exportar CSV' }).click();
 
     const download = await downloadPromise;
 
@@ -206,7 +206,7 @@ test('HU - 009 - Validar contenido del archivo CSV', async ({ page }) => {
     const csvContent = fs.readFileSync(downloadPath, 'utf-8');
 
     expect(csvContent).toContain('Servicios por día');
-    expect(csvContent).toContain('Mayo');
+    expect(csvContent).toContain('Junio');
   });
 });
 
@@ -221,36 +221,10 @@ test('HU - 009 - Mostrar mensaje cuando no existan datos', async ({ page }) => {
 
     });
     await test.step('Then el sistema debe indicar que no hay datos disponibles para ese periodo', async () => {
-        await page.getByText('Sin datos para mostrarDefine').click();
-        await page.getByText('Sin datos para mostrar').click();
+        await page.getByRole('textbox', { name: 'Fecha inicial' }).fill('2025-01-01');
+        await page.getByRole('textbox', { name: 'Fecha final' }).fill('2026-02-28');
+        await page.getByRole('button', { name: 'Generar reporte' }).click();
+        await page.getByText('Sin datos de género con los').click();
+        await page.getByText('Sin datos de etapa con los').click();
     });
-});
-
-// Servicios
-test('HU - 008 - Generar recibo al finalizar el proceso', async ({ page }) => {
-  qase.id(97);
-  await page.getByRole('button', { name: 'Servicios' }).click();
-  await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
-  await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('sof');
-  await page.locator('div').filter({ hasText: /^Sofio RamirezASEB-26-9002 · RASO120202MNLBBB02$/ }).first().click();
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('combobox').first().selectOption('Consultas');
-  await page.getByRole('combobox').nth(1).selectOption('75');
-  await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-26');
-  await page.getByRole('textbox', { name: 'Hora de cita' }).click();
-  await page.getByRole('textbox', { name: 'Hora de cita' }).fill('09:09');
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('combobox').selectOption('79');
-  await page.getByRole('button', { name: 'Agregar' }).click();
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('textbox', { name: '0.00' }).first().click();
-  await page.getByRole('textbox', { name: '0.00' }).first().fill('200');
-  await page.getByRole('combobox').selectOption('efectivo');
-  await page.getByRole('textbox', { name: '0.00' }).nth(1).click();
-  await page.getByRole('button', { name: 'Guardar' }).click();
-  await page.getByRole('button', { name: 'Recibos' }).click();
-  await page.getByRole('button', { name: 'Hoy' }).click();
-  await expect(page.getByRole('cell', { name: 'Sofio Ramirez' }).first()).toBeVisible();
-  await page.getByRole('cell', { name: 'CONSULTAS PSICOLOGÍA' }).first().click();
-  await page.getByRole('cell', { name: ':09' }).first().click();
 });
---
+++ Pruebas/tests/Regresion/test-Sprint2-ciclo2.spec.js
@@ -0,0 +1,228 @@
+import { test, expect } from '@playwright/test';
+import { qase } from 'playwright-qase-reporter';
+import path from 'path';
+import fs from 'fs';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://aebnl.netlify.app/login');
+  await page.getByRole('textbox', { name: 'Usuario' }).click();
+  await page.getByRole('textbox', { name: 'Usuario' }).fill('prueba1');
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('admin1');
+  await page.getByRole('button', { name: 'Entrar al sistema' }).click();
+});
+
+
+// Alertas
+test('HU - 026 - Clasificar correctamente las alertas por tipo', async ({ page }) => {
+    qase.id(79);
+        await test.step('Given que el usuario tiene una sesión activa And existen alertas de prerregistro, inventario y citas', async () => {
+        await page.getByRole('button', { name: '5' }).click();
+        await page.getByRole('button', { name: 'Terminación de una membresía' }).click();
+        await expect(page.getByRole('heading', { name: 'Gestion de Beneficiarios' })).toBeVisible();
+    });
+
+    await test.step('When accede al módulo de alertas', async () => {
+        await page.getByRole('button', { name: '5' }).click();
+        await page.getByRole('button', { name: 'Escasez de un producto en el' }).click();
+        await expect(page.getByRole('heading', { name: 'Inventario General' })).toBeVisible();
+    });
+
+    await test.step('Then cada alerta debe mostrarse dentro de su categoría correspondiente', async () => {
+        await page.getByRole('button', { name: '56' }).click();
+        await page.getByRole('button', { name: 'Prerregistros nuevos 0 Sin' }).click();
+    });
+
+    await test.step('And no debe mezclarse información entre tipos de alerta', async () => {
+        await expect(page.getByRole('heading', { name: 'Personas en pre-registro' })).toBeVisible();
+    });   
+});
+
+// Pagina de inicio
+test('HU - 019 - Redirigir al modulo de registro de servicios', async ({ page }) => {
+  qase.id(118);
+
+  await test.step('Given el usuario esta en el Menu de Inicio', async () => {
+    await expect(page.getByRole('button', { name: 'Registrar Servicio Documentar' })).toBeVisible();
+  });
+
+  await test.step('And el usuario puede visualizar el acceso rápido de registro de servicios', async () => {
+    await expect(page.getByRole('button', { name: 'Registrar Servicio Documentar' })).toBeVisible();
+  });
+
+  await test.step('Then el usuario puede dar clic al botón registrar servicio', async () => {
+    await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+  });
+
+  await test.step('And el sistema redirige a la pestaña de servicios', async () => {
+    await expect(page.getByRole('heading', { name: 'Registrar Nuevo Servicio' })).toBeVisible();
+  });
+});
+
+// Servicios
+test('HU - 008 - Registrar servicio con beneficiario por folio', async ({ page }) => {
+  qase.id(92);
+
+  await test.step('Given que el usuario inició sesión en el sistema', async () => {
+    await expect(page.getByRole('button', { name: 'Registrar Servicio Documentar' })).toBeVisible();
+  });
+
+  await test.step('And se encuentra en el módulo de servicios', async () => {
+    await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+    await expect(page.getByRole('heading', { name: 'Registrar Nuevo Servicio' })).toBeVisible();
+  });
+
+  await test.step('When selecciona registrar un nuevo servicio', async () => {
+    await expect(page.getByRole('textbox', { name: 'Nombre, folio o CURP...' })).toBeVisible();
+  });
+
+  await test.step('And elige un beneficiario mediante su folio', async () => {
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('9002');
+  });
+
+  await test.step('Then el sistema debe asociar el servicio al beneficiario correspondiente', async () => {
+    await expect(page.getByText('Sofia RamirezASEB-26-9002 ·')).toBeVisible();
+  });
+});
+
+test('HU - 008 - Registrar servicio con beneficiario por nombre', async ({ page }) => {
+  qase.id(93);
+
+  await test.step('Given que el usuario inició sesión en el sistema', async () => {
+    await expect(page.getByRole('button', { name: 'Registrar Servicio Documentar' })).toBeVisible();
+  });
+
+  await test.step('And se encuentra en el módulo de servicios', async () => {
+    await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+    await expect(page.getByRole('heading', { name: 'Registrar Nuevo Servicio' })).toBeVisible();
+  });
+
+  await test.step('When selecciona registrar un nuevo servicio', async () => {
+    await expect(page.getByRole('textbox', { name: 'Nombre, folio o CURP...' })).toBeVisible();
+  });
+
+  await test.step('And busca al beneficiario mediante su nombre', async () => {
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('Sofia');
+  });
+
+  await test.step('Then el sistema debe permitir seleccionar al beneficiario correspondiente', async () => {
+    await expect(page.getByText(/Sofia Ramirez.*ASEB-26-9002/)).toBeVisible();
+  });
+});
+
+test('HU - 008 - Guardar servicio con datos obligatorios', async ({ page }) => {
+  qase.id(94);
+
+  await test.step('Given que el usuario esta registrado un servicio', async() => {
+    await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+    await page.getByRole('button', { name: 'Citas del día' }).click();
+    await page.getByRole('button', { name: 'Por beneficiario' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('Sofia');
+      
+  });
+  await test.step('capturó el beneficiario, tipo de servicio, fecha y monto', async() => {
+    await page.locator('div').filter({ hasText: /^Sofia RamirezASEB-26-9002 · RASO120202MNLBBB02$/ }).nth(1).click();
+    await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+  await test.step('When guarda la información del servicio', async() => {
+    await page.getByRole('textbox', { name: 'Hora de cita' }).click();
+    await page.getByRole('textbox', { name: 'Hora de cita' }).fill('21:07');
+    await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-18');
+    await page.getByRole('combobox').first().selectOption('Procedimiento');
+    await page.getByRole('combobox').nth(1).selectOption('24');
+    await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+  await test.step('Then el sistema debe registrar el servicio correctamente', async() => {
+    await page.getByRole('combobox').selectOption('21');
+    await page.getByRole('button', { name: 'Agregar' }).click();
+    await page.getByRole('button', { name: 'Continuar' }).click();  
+  });
+});
+
+test('HU - 008 - Registrar servicio con consumo de materiales', async ({ page }) => {
+  qase.id(95);
+  await test.step('Given que el usuario esta registrado un servicio', async() => {
+      await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+      await page.getByRole('button', { name: 'Citas del día' }).click();
+      await page.getByRole('button', { name: 'Por beneficiario' }).click();
+      await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+      await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('Sofia');
+      await page.locator('div').filter({ hasText: /^Sofia RamirezASEB-26-9002 · RASO120202MNLBBB02$/ }).nth(1).click();
+      await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+
+  await test.step('When guarda servicio', async() => {
+    await page.getByRole('textbox', { name: 'Hora de cita' }).click();
+    await page.getByRole('textbox', { name: 'Hora de cita' }).fill('21:07');
+    await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-18');
+    await page.getByRole('combobox').first().selectOption('Procedimiento');
+    await page.getByRole('combobox').nth(1).selectOption('24');
+    await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+
+  await test.step('Then el sistema debe registrar el servicio correctamente', async() => {
+    await page.getByRole('combobox').selectOption('21');
+    await page.getByRole('button', { name: 'Agregar' }).click();
+    await page.getByRole('button', { name: 'Continuar' }).click();   
+  });
+});
+
+test('HU - 008 - Generar registro de pago correspondiente', async ({ page }) => {
+  qase.id(96);
+  await test.step('Given que el usuario registró un servicio correctamente', async() => {
+      await page.getByRole('button', { name: 'Registrar Servicio Documentar' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+    await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('sofia');
+    await page.locator('div').filter({ hasText: /^Sofia RamirezASEB-26-9002 · RASO120202MNLBBB02$/ }).nth(1).click();
+    await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+
+  await test.step('When confirma el registro del servicio', async() => {
+    await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-21');
+    await page.getByRole('textbox', { name: 'Hora de cita' }).click();
+    await page.getByRole('textbox', { name: 'Hora de cita' }).fill('21:05');
+    await page.getByRole('combobox').first().selectOption('Consultas');
+    await page.getByRole('combobox').nth(1).selectOption('146');
+    await page.getByRole('button', { name: 'Continuar' }).click();
+    await page.getByRole('combobox').selectOption('2');
+    await page.getByRole('button', { name: 'Agregar' }).click();
+    await page.getByRole('button', { name: 'Continuar' }).click();
+  });
+  await test.step('Then el sistema debe generar el registro de pago correspondiente', async() => {
+    await expect(page.getByText('Resumen del RegistroESTADO')).toBeVisible();
+  });
+});
+
+test('HU - 008 - Generar recibo al finalizar el proceso', async ({ page }) => {
+  qase.id(97);
+  await page.getByRole('button', { name: 'Servicios' }).click();
+  await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).click();
+  await page.getByRole('textbox', { name: 'Nombre, folio o CURP...' }).fill('sof');
+  await page.locator('div').filter({ hasText: /^Sofia RamirezASEB-26-9002 · RASO120202MNLBBB02$/ }).first().click();
+  await page.getByRole('button', { name: 'Continuar' }).click();
+  await page.getByRole('combobox').first().selectOption('Consultas');
+  await page.getByRole('combobox').nth(1).selectOption('75');
+  await page.getByRole('textbox', { name: 'Fecha' }).fill('2026-05-26');
+  await page.getByRole('textbox', { name: 'Hora' }).click();
+  await page.getByRole('textbox', { name: 'Hora' }).fill('09:09');
+  await page.getByRole('button', { name: 'Continuar' }).click();
+  await page.getByRole('combobox').selectOption('79');
+  await page.getByRole('button', { name: 'Agregar' }).click();
+  await page.getByRole('button', { name: 'Continuar' }).click();
+  await page.getByRole('textbox', { name: '0.00' }).first().click();
+  await page.getByRole('textbox', { name: '0.00' }).first().fill('200');
+  await page.getByRole('combobox').selectOption('efectivo');
+  await page.getByRole('textbox', { name: '0.00' }).nth(1).click();
+  await page.getByRole('button', { name: 'Guardar' }).click();
+  await page.getByRole('button', { name: 'Recibos' }).click();
+  await page.getByRole('button', { name: 'Hoy' }).click();
+  await expect(page.getByRole('cell', { name: 'Sofia Ramirez' }).first()).toBeVisible();
+  await page.getByRole('cell', { name: 'CONSULTAS PSICOLOGÍA' }).first().click();
+  await page.getByRole('cell', { name: ':09' }).first().click();
+});
+
+
+
