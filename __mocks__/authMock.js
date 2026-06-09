3d74e8294147b44e047c1f2ac7c2e1d2c47ed439
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-06-09 01:21:54 -0600

    citas pop call

--- __mocks__/authMock.js
+++ __mocks__/authMock.js
@@ -1,4 +1,9 @@
-const authFetch = jest.fn((url, options) =>
-  options !== undefined ? globalThis.fetch(url, options) : globalThis.fetch(url)
-);
+const authFetch = jest.fn((url, options) => {
+  if (options === undefined) return globalThis.fetch(url);
+  const headers = { ...(options.headers || {}) };
+  if (options.body && !headers['Content-Type'] && !headers['content-type']) {
+    headers['Content-Type'] = 'application/json';
+  }
+  return globalThis.fetch(url, { ...options, headers });
+});
 module.exports = { authFetch };
