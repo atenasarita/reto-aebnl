cc9151bdeb990a3d6cbdb7785e446ffa293a9242
atenasarita <150080590+atenasarita@users.noreply.github.com>
2026-06-09 16:35:46 -0600

    fix: step registro de servicios issues

--- client/src/components/layout/servicios/Registro/StepInsumos.jsx
+++ client/src/components/layout/servicios/Registro/StepInsumos.jsx
@@ -170,7 +170,7 @@ export default function StepInsumos({ insumos, setInsumos }) {
               productoSelec
                 ? `$${
                     productos.find(
-                      (p) => p.id === parseInt(productoSelec)
+                      (p) => p.id === Number.parseInt(productoSelec)
                     )?.precio ?? ""
                   }`
                 : ""
